5c946a6ffdf0d302ca2e37f30a293a4cf8ac053a
Ratik Sharma <Ratik96@gmail.com>
2017-05-16 01:41:46 +0530

    Add meta tags and tweak  scrollTo

--- js/site.js
+++ js/site.js
@@ -35,5 +35,5 @@ $(document).on('click', 'a[href^="#"]', function(e) {
     var pos = $id.offset().top;
 
     // animated top scrolling
-    $('body, html').animate({scrollTop: pos});
+    $('body, html').animate({scrollTop: pos}, 1000);
 });
